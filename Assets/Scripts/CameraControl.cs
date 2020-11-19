1ea0ad75c0f36ca1ccbfb9f0ab21659374749b38
Arath-Penca <50471854+Arath-Penca@users.noreply.github.com>
2020-11-18 22:05:32 -0500

    Camera that follows player
    Camera is still a bit buggy while i figure out how to work with it.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform cameraObject;
+    public float cameraYOffset = 0.0f;
+    public float cameraSmoothness = 0.3f;
+    public int cameraRefreshRate = 60;
+    public int cameraWidth = 400;
+    public int cameraHeight = 400;
+    public bool isFullscreen = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(cameraWidth, cameraHeight, isFullscreen, cameraRefreshRate);
+        cameraYOffset = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(cameraObject.position.x, cameraObject.position.y, cameraYOffset);
+        transform.position = Vector3.Slerp(cameraObject.transform.position, newPosition, cameraSmoothness);
+    }
+}
